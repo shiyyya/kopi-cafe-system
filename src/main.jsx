59f1926132db0e8b1f220c54841fe5f5a3f82f7a
shunshine-strawberry <bedayoshunshine226@gmail.com>
2026-09-01 15:11:11 +0800

    pacheck

--- src/main.jsx
+++ src/main.jsx
@@ -8,11 +8,10 @@ import StoreLocator from './customer/pages/store-locator/store-locator.jsx';
 import OrderStatus from './customer/pages/order-status/order-status.jsx';
 import OrderHistory from './customer/pages/order-history/order-history.jsx';
 import Settings from './customer/pages/settings/settings.jsx';
-import LogIn from './customer/pages/Login/Login.jsx';
-import SignUp from './customer/pages/signup/signup.jsx';
+import PlaceOrder from './customer/pages/place-order/place-order.jsx';
 import DeliveryEligibility from './components/blocks/delivery-eligibility/delivery-eligibility.jsx';
-// import Walkin_Customer from './staff/pages/walk-in/walk-in.jsx';
-
+import Login from './components/cards/login/login.jsx';
+import Signup from './components/cards/signup/signup.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -24,12 +23,12 @@ createRoot(document.getElementById('root')).render(
           <Route path="order-status" element={<OrderStatus />} />
           <Route path="order-history" element={<OrderHistory />} />
           <Route path="settings" element={<Settings />} />
-           <Route path="login" element={<LogIn />} />
-           <Route path="signup" element={<SignUp />} />
-            <Route path="delivery-eligibility" element={<DeliveryEligibility />} />
-           {/* <Route path="walk-in" element={<Walkin_Customer />}/> */}
+          <Route path="place-order" element={<PlaceOrder />} />
+          <Route path="delivery-eligibility" element={<DeliveryEligibility />} />
+            <Route path="login" element={<Login />} />
+            <Route path="signup" element={<Signup />} />
         </Route>
       </Routes>
     </BrowserRouter>
-  </StrictMode>,
-)
+  </StrictMode>
+);
