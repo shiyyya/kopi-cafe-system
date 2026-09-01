170f4ebc676623bf9e40b5cafbe851587812a7d8
shunshine-strawberry <bedayoshunshine226@gmail.com>
2026-09-01 08:54:51 +0800

    main/app

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,10 @@ import StoreLocator from './customer/pages/store-locator/store-locator.jsx';
 import OrderStatus from './customer/pages/order-status/order-status.jsx';
 import OrderHistory from './customer/pages/order-history/order-history.jsx';
 import Settings from './customer/pages/settings/settings.jsx';
+import LogIn from './customer/pages/Login/Login.jsx';
+import SignUp from './customer/pages/signup/signup.jsx';
+import Walkin_Customer from './staff/pages/walk-in/walk-in.jsx';
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -19,6 +23,15 @@ createRoot(document.getElementById('root')).render(
           <Route path="order-status" element={<OrderStatus />} />
           <Route path="order-history" element={<OrderHistory />} />
           <Route path="settings" element={<Settings />} />
+           <Route path="login" element={<LogIn />} />
+           <Route path="signup" element={<SignUp />} />
+           <Route path="walk-in" element={<Walkin_Customer />}/>
+           <Route>
+          </Route>
+           <Route>
+            <Route>
+          </Route>
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
