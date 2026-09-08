94d431b54361bb5e66895697dc4697a02c0c5cc4
shunshine-strawberry <bedayoshunshine226@gmail.com>
2026-09-09 00:41:33 +0800

    staff

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
          <Outlet />
       
       </div>
-      {/*<Link to="store-locator">Store Locator</Link><br />
+      <Link to="store-locator">Store Locator</Link><br />
       <Link to="settings">Settings</Link><br />
       <Link to="order-status">Order Status</Link><br />
       <Link to="order-history">Order History</Link><br />
@@ -17,7 +17,7 @@ function App() {
       <Link to="login">Login</Link><br />
       <Link to="signup">Sign Up</Link><br />
       <Link to="qr-payment">QR Payment</Link><br />
-      <Link to="payment-confirmation">Payment Confirmation</Link><br />*/}
+      <Link to="payment-confirmation">Payment Confirmation</Link><br />
     </div>
   )
 }
